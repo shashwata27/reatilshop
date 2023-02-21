0f509a2d1774eedb1583f57e6f7287c340a8feb8
Shashwata Saha <sahas277@gmail.com>
2023-02-21 11:17:58 +0530

    add. Integration Test for Customers API

---
+++ src/test/java/com/pet/project/retailshop/controller/CustomerControllerIntegrationTest.java
@@ -0,0 +1,89 @@
+package com.pet.project.retailshop.controller;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CustomerControllerIntegrationTest {
+    private static final String CUSTOMERS_API_GENERAL_URL="/v1/customers";
+
+    @Autowired
+    private TestRestTemplate template;
+
+    @Test
+    void getCustomersById_IntegrationTest() throws JSONException {
+        String expectedCustomerById=
+                """
+                {
+                  "customer_id": 1,
+                  "name": "Raimondo Aire",
+                  "address": "03949 Warrior Junction",
+                  "phoneNumber": "999-564-3927"
+                }
+                """;
+
+        ResponseEntity<String> actualCustomerById = template.getForEntity(CUSTOMERS_API_GENERAL_URL + "/1", String.class);
+        HttpStatusCode actualCustomerByIdStatusCode = actualCustomerById.getStatusCode();
+        String actualCustomerByIdContentType = actualCustomerById.getHeaders().get("Content-Type").get(0);
+        String actualCustomerByIdBody = actualCustomerById.getBody();
+
+        assertTrue(actualCustomerByIdStatusCode.is2xxSuccessful());
+        assertEquals("application/json",actualCustomerByIdContentType);
+        JSONAssert.assertEquals(expectedCustomerById, actualCustomerByIdBody,true);
+    }
+
+    @Test
+    void getCustomersByName_IntegrationTest() throws JSONException {
+        String expectedCustomerByName=
+                """
+                                {
+                                   "customer_id": 2,
+                                   "name": "Shanna Frodsam",
+                                   "address": "90866 Hauk Avenue",
+                                   "phoneNumber": "360-274-4713"
+                                 }
+                        """;
+
+        ResponseEntity<String> actualCustomerByName = template.getForEntity(CUSTOMERS_API_GENERAL_URL + "/name/Shanna Frodsam", String.class);
+        HttpStatusCode actualCustomerByNameStatusCode = actualCustomerByName.getStatusCode();
+        String actualCustomerByNameContentType = actualCustomerByName.getHeaders().get("Content-Type").get(0);
+        String actualCustomerByNameBody = actualCustomerByName.getBody();
+
+        assertTrue(actualCustomerByNameStatusCode.is2xxSuccessful());
+        assertEquals("application/json",actualCustomerByNameContentType);
+        JSONAssert.assertEquals(expectedCustomerByName, actualCustomerByNameBody,true);
+    }
+
+    @Test
+    void getAllCustomersByPhoneNumber_IntegrationTest() throws JSONException {
+        String expectedCustomerByPhoneNumber=
+                """
+                                [
+                                    {
+                                      "customer_id": 5,
+                                      "name": "Anderson Rawcliffe",
+                                      "address": "03 Dawn Lane",
+                                      "phoneNumber": "141-665-3225"
+                                    }
+                                  ]
+                        """;
+
+        ResponseEntity<String> actualCustomerByPhoneNumber = template.getForEntity(CUSTOMERS_API_GENERAL_URL + "/phone-number/141-665-3225", String.class);
+        HttpStatusCode actualCustomerByPhoneNumberStatusCode = actualCustomerByPhoneNumber.getStatusCode();
+        String actualCustomerByPhoneNumberContentType = actualCustomerByPhoneNumber.getHeaders().get("Content-Type").get(0);
+        String actualCustomerByPhoneNumberBody = actualCustomerByPhoneNumber.getBody();
+
+        assertTrue(actualCustomerByPhoneNumberStatusCode.is2xxSuccessful());
+        assertEquals("application/json",actualCustomerByPhoneNumberContentType);
+        JSONAssert.assertEquals(expectedCustomerByPhoneNumber, actualCustomerByPhoneNumberBody,true);
+    }
+
+}
